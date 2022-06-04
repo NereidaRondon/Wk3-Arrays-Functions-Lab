d2ce7800909695929300d6b92df5d80d1c49804f
Nereida Rondon <97356401+NereidaRondon@users.noreply.github.com>
2022-06-03 22:03:45 -0400

    Done

--- ArraysAndFunctions.js
+++ ArraysAndFunctions.js
@@ -46,37 +46,51 @@ var ourTodoList = [];
 ourTodoList = myTodoList.concat(yourTodoList);
 console.log(ourTodoList);
 
+//Solution from Instructor
+ourTodoList = [...myTodoList, ...yourTodoList];
+console.log(ourTodoList);
+
 console.log("Sort by Alphabetical order");
 // sort the following array from Z-A
 ourTodoList.sort();
 ourTodoList.reverse();
 console.log(ourTodoList);
 
-/*ourTodoList.sort(function (a, b) {
-  return b - a;
-});
-console.log(ourTodoList);*/
-
 // create a function called reverse that takes in parameter
+// this function will return the opposite of whatever is passed in
+// if its a boolean, return the opposite
+// if its a number, or a string, return the reverse (i.e. 1234 becomes 4321, Name becomes emaN)
+// if its an array, return the reversed array with each element reversed
 let numbers = [1, 5, 4, 3, 2, 6, 3, 1];
 console.log("Array in Reverse");
 numbers.sort();
 numbers.reverse();
 console.log(numbers);
 
-function reverse() {}
-// this function will return the opposite of whatever is passed in
-// if its a boolean, return the opposite
-// if its a number, or a string, return the reverse (i.e. 1234 becomes 4321, Name becomes emaN)
-// if its an array, return the reversed array with each element reversed
+function reverse(x) {}
 
 // create a function called addingMachine that will add all passed numbers and return the total
 // Note: you don't know how many numbers will be passed
-
-let addingMachine = numbers.reduce(function (accumulator, currentValue) {
+let addingMachine1 = numbers.reduce(function (accumulator, currentValue) {
   return accumulator + currentValue;
 });
-console.log(addingMachine);
+console.log(addingMachine1);
+
+//Solution from Instructor
+let arguements = [1, 2, 3, 4]; //???
+
+function addingMachine() {
+  console.log(arguments); //???
+  let sum = 0;
+  for (let i = 0; i < arguements.length; i++) {
+    let number = arguments[i];
+    sum += number;
+  }
+  return sum;
+}
+console.log(addingMachine(1, 2, 3, 4));
+// console.log(addingMachine(1,2,3,4,5,6,7));
+// console.log(addingMachine(1,2,3,4,5,6,7,7,9,4,2344,112,34343,234));
 
 // You just signed a contract as an estimator for a restoration company.
 // Your contract states that you take home 10% of the profits on the first $100,000
@@ -85,3 +99,43 @@ console.log(addingMachine);
 // 40% on all profits above $1,000,000
 // create a function that will allow you to check how much of a bonus you make
 // the function should take in two variables as arguments, grossInvoiced and profitMargin
+
+function myBonus(grossInvoiced, profitMargin) {
+  let profits = grossInvoiced * profitMargin;
+
+  let bonus = 0;
+
+  let tier1Cieling = 100000;
+  let tier2Cieling = 400000;
+  let tier3Cieling = 500000;
+
+  let tier1Rate = 0.1;
+  let tier2Rate = 0.2;
+  let tier3Rate = 0.35;
+  let tier4Rate = 0.4;
+
+  let maxBonusTier1 = tier1Cieling * tier1Rate;
+  let maxBonusTier2 = tier2Cieling * tier2Rate + maxBonusTier1;
+  let maxBonusTier3 = tier3Cieling * tier3Rate + maxBonusTier2;
+
+  if (profits <= tier1Cieling) {
+    bonus = profits * 0.1;
+  } else if (profits <= tier2Cieling + tier1Cieling) {
+    bonus = maxBonusTier1 + (profits - tier1Cieling) * tier2Rate;
+  } else if (profits <= tier3Cieling + tier2Cieling + tier1Cieling) {
+    bonus = maxBonusTier2 + (profits - tier1Cieling - tier2Cieling) * tier3Rate;
+  } else if (profits > 1000000) {
+    bonus =
+      maxBonusTier3 +
+      (profits - tier1Cieling - tier2Cieling - tier3Cieling) * tier4Rate;
+  } else {
+    return "Error";
+  }
+
+  return bonus;
+}
+
+console.log(myBonus(10000, 0.563));
+console.log(myBonus(250000, 0.356));
+// console.log(myBonus(750000, .235))
+// console.log(myBonus(35000000, .487))
